64901a3e818c1927073b151e6314adaeba724122
Lenar Gilyazov <lenar8553@gmail.com>
2021-11-17 18:29:39 +0300

    Release 3.0.1

---
+++ MovikaSDK.xcframework/ios-arm64_x86_64-simulator/MovikaSDK.framework/Headers/MovikaSDK.h
@@ -0,0 +1,18 @@
+//
+//  GameLogic.h
+//  GameLogic
+//
+//  Created by Ishmukhametov on 15/04/2019.
+//  Copyright © 2019 Interactive Platform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for GameLogic.
+FOUNDATION_EXPORT double GameLogicVersionNumber;
+
+//! Project version string for GameLogic.
+FOUNDATION_EXPORT const unsigned char GameLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameLogic/PublicHeader.h>
+
+
